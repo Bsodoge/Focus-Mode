79f53df4977acb8f310a48c9e5cf58663318543f
bsodoge <kashhh4u@gmail.com>
2025-03-11 23:07:30 +0000

    Add basic functionality

--- src/js/contentscript.js
+++ src/js/contentscript.js
@@ -1,17 +1,29 @@
-let isToggled = false;
-
 let settings = {
-    isToggled
+    isToggled: undefined,
+	links: undefined,
+	days: undefined,
+	startTime: undefined,
+	endTime: undefined
 }
 
 const applySettings = (storageSettings) => {
-	if (storageSettings.isToggled) console.log("Hi"); 
+	if (storageSettings.isToggled) settings = storageSettings; 
 }
 
-const onLoad = () => {
-	browser.storage.local.get(settings).then((storageSettings) => {
-		applySettings(storageSettings);
-	});
+const blockSite = () => {
+	const day = new Date().getDay();
+	const time = new Date().toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"});
+	console.log(time);
+	if(settings.links.includes(window.location.href) && settings.days.includes(day) && (time >= settings.startTime) && (time <= settings.endTime) ) close();
+}
+
+const onLoad = async () => {
+	const storageSettings = await browser.storage.local.get(settings);
+	applySettings(storageSettings);
+	console.log(settings);
+	blockSite();
 }
 
 browser.runtime.onMessage.addListener(applySettings);
+
+onLoad();
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,10 +1,42 @@
 let isToggled = false;
 let optionsToggled = false;
+let links = [];
+let days = [];
+let startTime = new Date().toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"});
+let endTime = new Date().toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"});
 let settings = {
     isToggled,
+    links,
+    days,
+    startTime,
+    endTime
 }
 
 const buttonToggle = document.getElementById("button");
+const linkInputButton = document.getElementById("link-submit");
+const linkInput = document.getElementById("input-link")
+const errorOutput = document.getElementById("error"); // change this to a more modern version later
+const listContainer = document.getElementById("link-list");
+const daysContainer = document.getElementById("days-container");
+const startInput = document.getElementById("start");
+const endInput = document.getElementById("end");
+
+const addLink = (link) => {
+    if (!validateUrl(link)){
+        errorOutput.style.display = "block";
+        return;
+    }
+    if (!links.includes(link)) {
+        links.push(link);
+        errorOutput.style.display = "none";
+        listLinks();
+        setSettings();
+    }
+}
+
+function validateUrl(value) { //courtesy of https://stackoverflow.com/questions/8667070/javascript-regular-expression-to-validate-url/15855457?r=Saves_UserSavesList#15855457
+  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+}
 
 const changeButtonText = () => {
     if (!isToggled) {
@@ -16,9 +48,36 @@ const changeButtonText = () => {
     buttonToggle.classList.add('activated')
 }
 
+const removeLink = (container) => {
+    links = links.filter((link, id) => id != container.getAttribute("id"));
+    container.remove();
+    setSettings();
+}
+
+const listLinks = () => {
+    while(listContainer.hasChildNodes()){
+        listContainer.removeChild(listContainer.firstChild);
+    }
+    links.forEach((link, index) => {
+        const container = document.createElement("div");
+        const span = document.createElement("span");
+        const button = document.createElement("button");
+        container.setAttribute("id", index);
+        button.addEventListener("click", () => removeLink(container));
+        span.innerText = link;
+        container.append(span);
+        container.append(button);
+        listContainer.append(container);
+    });
+}
+
 const setSettings = () => {
     settings = {
         isToggled,
+        links,
+        days,
+        startTime,
+        endTime
     }
     browser.storage.local.set(settings);
 }
@@ -26,7 +85,12 @@ const setSettings = () => {
 const onOpen = () => {
     browser.storage.local.get(settings).then((storageSettings) => {
         isToggled = storageSettings.isToggled;
+        links = storageSettings.links;
+        days = storageSettings.days;
+        startTime = storageSettings.startTime;
+        endTime = storageSettings.endTime;
         changeButtonText();
+        listLinks();
         settings = storageSettings;
         browser.tabs.query({ active: true, currentWindow: true }, sendMessage);
     });
@@ -43,6 +107,27 @@ const toggleExtension = (tabs) => {
     sendMessage(tabs);
 }
 
+const handleDay = (button) => {
+    givenDay = parseInt(button.target.id);
+    if(days.includes(givenDay)){
+        days = days.filter((day) => day != givenDay);
+        setSettings();
+        return;
+    }
+    days.push(givenDay);
+    setSettings();
+}
+
+const handleTime = (input) => {
+    if(input.target.id === "start") startTime = input.target.value
+    if(input.target.id === "end") endTime = input.target.value
+    setSettings();
+}
+
 buttonToggle.addEventListener("click", () => browser.tabs.query({active: true, currentWindow: true}, toggleExtension));
+linkInputButton.addEventListener("click", () => addLink(linkInput.value));
+daysContainer.childNodes.forEach((child) => child.addEventListener("click", handleDay));
+startInput.addEventListener("change", handleTime);
+endInput.addEventListener("change", handleTime);
 
 onOpen();
