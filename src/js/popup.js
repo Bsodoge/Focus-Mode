4b73504ab61cb25e7b4237b0c9f903d1d03be970
bsodoge <kashhh4u@gmail.com>
2025-03-16 01:48:02 +0000

    Update popup js

--- src/js/popup.js
+++ src/js/popup.js
@@ -33,31 +33,24 @@ const specialChars = {
 const buttonToggle = document.getElementById("button");
 const linkInputButton = document.getElementById("link-submit");
 const linkInput = document.getElementById("input-link")
-const errorOutput = document.getElementById("error"); // change this to a more modern version later
 const listContainer = document.getElementById("link-list");
 const daysContainer = document.getElementById("days-container");
 const startInput = document.getElementById("start");
 const endInput = document.getElementById("end");
 
 const addLink = (link) => {
-    if (!validateUrl(link)){
-        errorOutput.style.display = "block";
+    if (links.includes(link)){
         return;
     }
     displayLinks.push(link);
     link = convertToRegex(link);
     if (!links.includes(link)) {
         links.push(link);
-        errorOutput.style.display = "none";
         listLinks();
         setSettings();
     }
+    linkInput.value = "";
 }
-
-function validateUrl(value) { //courtesy of https://stackoverflow.com/questions/8667070/javascript-regular-expression-to-validate-url/15855457?r=Saves_UserSavesList#15855457
-  return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
-}
-
 const changeButtonText = () => {
     if (!isToggled) {
         buttonToggle.innerText = "Activate";
